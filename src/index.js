1bc21ea25adffda4a4ccc6dc7d8b4a6939bd097d
Dmitriy577 <51756301+Dmitriy577@users.noreply.github.com>
2022-09-04 19:47:26 +0300

    5

--- src/index.js
+++ src/index.js
@@ -1,133 +1,77 @@
-import photoCard from "./templates/photo-card.hbs"
-import SimpleLightbox from "simplelightbox"
-import "simplelightbox/dist/simple-lightbox.min.css"
-import { Notify } from 'notiflix/build/notiflix-notify-aio'
-import "notiflix/dist/notiflix-3.2.5.min.css"
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import fetchSearch from "./js/pixabayApi"
+import { refs } from './js/refs';
+import { getGallerydData } from './js/getGalleryData';
 
-const searchFormRef = document.querySelector(`#search-form`)
-const galleryRef = document.querySelector(`.gallery`)
-const loadMoreBtn = document.querySelector('.btn-load-more')
-const toBtnTop = document.querySelector('.btn-to-top')
-const loading = document.querySelector('.loading')
-const inputRef = document.querySelector(`[name="searchQuery"]`)
+import createMarkUp from './templates/imgCardMarkUp.hbs';
 
-let simpleLightBox = null
-let page = 1
-let perPage = 40
-let q = ''
-let totalPages = 0
+const searchParams = {
+  params: {
+    q: null,
+    page: 1,
+    per_page: 40,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  },
+};
+let maxPage = 1;
 
-window.addEventListener('scroll', onScroll)
-toBtnTop.addEventListener('click', onToTopBtn)
-searchFormRef.addEventListener("submit", onSubmit)
+const observerOptions = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 1,
+};
 
-function onToTopBtn() {
-    if (window.pageYOffset > 0) {
-      window.scrollTo({ top: 0, behavior: 'smooth' })
-    }
+const observer = new IntersectionObserver(galleryUpdate, observerOptions);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
+
+refs.form.addEventListener('submit', onSearchSubmit);
+
+async function onSearchSubmit(e) {
+  e.preventDefault();
+  if (
+    !e.currentTarget.elements.searchQuery.value ||
+    searchParams.params.q === e.currentTarget.elements.searchQuery.value
+  )
+    return;
+
+  refs.gallery.innerHTML = '';
+  searchParams.params.q = e.currentTarget.elements.searchQuery.value;
+  e.currentTarget.elements.searchQuery.value = '';
+  searchParams.params.page = 1;
+  const res = await getGallerydData(searchParams);
+
+  if (!res.data.hits.length) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  refs.gallery.insertAdjacentHTML('beforeend', createMarkUp(res.data));
+  maxPage = Math.ceil(res.data.totalHits / searchParams.params.per_page);
+  observer.observe(refs.scrollPoint);
+  gallery.refresh();
+  Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
+}
+
+async function galleryUpdate(enteries) {
+  if (refs.gallery.innerHTML == '') return;
+  if (!enteries[0].isIntersecting) return;
+  if (searchParams.params.page == maxPage) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return;
   }
 
-function onSubmit(ev){
-    ev.preventDefault()
-    if(inputRef.getAttribute("disabled")){
-        return
-    }
-    q = inputRef.value.trim()
-    loadMoreBtn.classList.add('is-hidden')
-    loading.classList.remove('show')
-    galleryRef.innerHTML = ''
-    if(!q){
-        return
-    }
-    inputRef.setAttribute("disabled","disabled")
-    page = 1
-    totalPages = 0
-    load()
-}
-
-function onScroll() {
-    const scrolled = window.pageYOffset
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
-
-    if (scrolled > clientHeight) {
-        toBtnTop.classList.add('btn-to-top--visible')
-    }
-    if (scrolled < clientHeight) {
-        toBtnTop.classList.remove('btn-to-top--visible')
-    }
-    let isDisabled = inputRef.getAttribute("disabled")
-    if(isDisabled){
-        return
-    }
-    if (clientHeight + scrollTop >= scrollHeight - 5 && page > 0) {
-        showLoading()
-    }
-}
-
-function showLoading() {
-    if(loading.classList.contains('show')){
-        return 
-    }
-    loading.classList.add('show')
-    inputRef.setAttribute("disabled","disabled")
-	setTimeout (onLoadMoreBtn, 2000)
-}
-
-function onLoadMoreBtn() {
-    page += 1
-    if (page > totalPages) {
-        loadMoreBtn.classList.add('is-hidden')
-        inputRef.removeAttribute("disabled")
-        loading.classList.remove('show')
-        Notify.warning('We are sorry, but you have reached the end of search results.')
-        page=0
-        return
-    }
-    load()
-}
-
-function load(){
-    fetchSearch(q, page, perPage)
-    .then(responseData => {
-        if(page === 1){
-            galleryRef.innerHTML = ''
-        }
-        if (responseData.data && (!responseData.data.total || !responseData.data.hits.length)){
-            Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-            return 
-        }
-        totalPages = Math.ceil(responseData.data.total / perPage)
-        if(page === 1){
-            Notify.success(`Hooray! We found ${responseData.data.total} images.`)
-        }
-        renderGallery(responseData.data.hits)
-        loadMoreBtn.classList.toggle('is-hidden', !(responseData.data.total > perPage))
-        if(!simpleLightBox){
-            simpleLightBox = new SimpleLightbox('.gallery a')
-        } else {
-            simpleLightBox.refresh()
-        }
-    })
-    .catch(error => {
-        Notify.failure("Something bad happened. Check the browser console")
-        console.warn(error)
-    })
-    .finally(() => {
-        setTimeout(() => {
-            inputRef.removeAttribute("disabled")
-            loading.classList.remove('show')
-        }, 500)
-    })
-}
-
-function renderGallery(images) {
-    const markup = images.map(image => {
-        //const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image
-        const markup = photoCard(image)
-        return markup
-    }).join('')
-
-    galleryRef.insertAdjacentHTML('beforeend', markup)
+  searchParams.params.page += 1;
+  const res = await getGallerydData(searchParams);
+  refs.gallery.insertAdjacentHTML('beforeend', createMarkUp(res.data));
+  gallery.refresh();
 }
